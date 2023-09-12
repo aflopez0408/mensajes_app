19cb2af75f6cebbd30d3497b28a9e29a9a39c140
aflopez0408 <74578619+aflopez0408@users.noreply.github.com>
2023-09-11 22:00:05 -0500

    Dia 1 App mensaje

--- src/main/java/com/mycompany/mensajes_app/Inicio.java
+++ src/main/java/com/mycompany/mensajes_app/Inicio.java
@@ -5,6 +5,7 @@
 package com.mycompany.mensajes_app;
 
 import java.sql.Connection;
+import java.util.Scanner;
 
 /**
  *
@@ -14,12 +15,39 @@ public class Inicio {
 
     public static void main(String[] args) {
         
-        Conexion conexion = new Conexion();
+        Scanner sc = new Scanner(System.in);
+        int opcion=0;
+        do {
+            System.out.println("-----------------");
+            System.out.println("Aplicacion de mensajes");
+            System.out.println("1. crear de mensaje");
+            System.out.println("2. listar mensajes");
+            System.out.println("3. editar mensaje");
+            System.out.println("4. eliminar mensaje");
+            System.out.println("5. salir");
+             /// leemos la opcion del usuario
+            opcion = sc.nextInt();
+            
+            switch(opcion){
+                case 1:
+                    mensajesServices.crearMensaje();
+                    break;
+                case 2:
+                    mensajesServices.listarMensaje();
+                    break;
+                case 3:
+                    mensajesServices.borrarMensaje();
+                    break;  
+                case 4:
+                    mensajesServices.editarMensaje();
+                    break;
+                default:
+                    break;
+            }
+        }while(opcion !=5);
         
-        try(Connection cnx = conexion.get_connection()){
-        }catch(Exception e){
-            System.out.println(e);
-        }
+       
+        
         
     }
 }
--- src/main/java/com/mycompany/mensajes_app/MensajesServices.java
+++ src/main/java/com/mycompany/mensajes_app/MensajesServices.java
@@ -4,10 +4,30 @@
  */
 package com.mycompany.mensajes_app;
 
+import java.util.Scanner;
+
 /**
  *
  * @author anfer
  */
-public class MensajesServices {
+public class mensajesServices {
+    
+    public static void crearMensaje(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe tu mensaje");
+        String mensaje = sc.nextLine();
+        
+        System.out.println("tu nombre");
+        String nombre = sc.nextLine();
+        
+        Mensajes registro = new Mensajes();
+        registro.setMensaje(mensaje);
+        registro.setAutor_mensaje(nombre);
+        MensajesDAO.crearMensajeDB(registro);
+        
+    }
+    public static void listarMensaje(){}
+    public static void borrarMensaje(){}
+    public static void editarMensaje(){}
     
 }
